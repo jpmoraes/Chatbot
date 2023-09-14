5205ea1996307d3154a3b504cac4051fe0eee92a
Joao Moraes <joao.moraes@ba.senac.br>
2023-09-14 11:47:25 -0300

    Versão 1

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://code.jquery.com/jquery-1.9.1.js"></script>
+    <script src="main.js" defer></script>   
+    <title>ChatBot</title>
+</head>
+<body>
+    <h1>ChatBot</h1>
+    <div id="chat">
+        <div id="chat-area"></div>
+        <p id="resposta"></p><br>
+        <input type="text" id="pergunta" placeholder="Digite sua pergunta...">
+        <button id="perguntar">Enviar</button>
+    </div>
+  
+</body>
+</html>
