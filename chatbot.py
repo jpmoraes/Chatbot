f7557197022e604827faa5482f4508cea4180765
jpmoraes <morais566@gmail.com>
2023-09-25 09:52:04 -0300

    update 01

--- chatbot.py
+++ chatbot.py
@@ -1,18 +1,21 @@
 from flask import Flask, request, jsonify
 import random
+from nltk.tokenize import word_tokenize
 import nltk
 
 nltk.download('punkt')
 
 # Pares de perguntas e respostas
+
 perguntas_respostas = {
-    "Olá": "Olá! Como posso ajudar você?",
-    "Como você está?": "Estou bem, obrigado por perguntar.",
-    "Qual é o seu nome?": "Meu nome é ChatBot.",
-    "O que você pode fazer?": "Posso responder a perguntas simples.",
-    "Tchau": "Tchau! Tenha um bom dia!"
+    1: "Olá! Como posso ajudar você?",
+    2: "Estou bem, obrigado por perguntar.",
+    3: "Meu nome é Dira.",
+    4: "Posso responder a perguntas simples.",
+    5: "Tchau! Tenha um bom dia!"
 }
 
+Cumprimento = ["Olá", "oi", "e ai"," qual foi","fala"]
 
 
 app = Flask(__name__)
@@ -24,19 +27,26 @@ def chatbot():
     resposta = responder_pergunta(user_question)
     return jsonify({'response': resposta})
 
+
 def responder_pergunta(pergunta):
-    resposta = perguntas_respostas.get(pergunta)
-    if resposta:
-        return resposta
+   
+    tokens = word_tokenize(pergunta.lower())
+
+    if(len(tokens) > 1):
+        expectativa = (tokens[0] +" "+ tokens[1]) 
+        if expectativa in Cumprimento:
+            resposta = perguntas_respostas.get(1)      
     else:
-        return "Desculpe, não entendi a pergunta."
+        for word in tokens:
+                
+                if word in Cumprimento:
+                    resposta = perguntas_respostas.get(1)        
+                    if resposta:
+                        return resposta
+                    else:
+                        return "Desculpe, não entendi a pergunta."
+                    break
 
 
 if __name__ == '__main__':
     app.run(debug=True, host='localhost', port=5000)
-
-
-
-
-
-
