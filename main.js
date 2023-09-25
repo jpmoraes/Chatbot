e83e0642ab4a735284b1d676a7caea6d62304072
jpmoraes <morais566@gmail.com>
2023-09-25 10:58:53 -0300

    update 02

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 $(document).ready(function () {
     $("#perguntar").click(function () {
@@ -34,40 +33,3 @@ $(document).ready(function () {
 
     });
 });
-=======
-
-$(document).ready(function () {
-    $("#perguntar").click(function () {
-  
-        var pergunta = $("#pergunta").val(); 
-        console.log(pergunta);
-        $.ajax({
-            url: 'conectChat.php',
-            method: 'post',
-            dataType: 'json',
-            data: {
-                pergunta:pergunta                
-            },
-            success: function(data) {
-                console.log(data);
-                 if(data.status != "error"){        
-                    var response = document.getElementById('resposta');
-                    response.textContent = data;
-                 }else{
-                    $("#resposta").val("Erro");
-                 }       
-            
-            },
-            error: function(xhr, status, error) {
-                console.log(xhr.responseText);
-                
-            }
-        });
-
-
-
-
-
-    });
-});
->>>>>>> 1aa57ca8d2fcccb3ceafc9e2faf9488439827ee2
